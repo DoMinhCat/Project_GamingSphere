fda8613bbd9bd5af1c7079bd34b0cd4c0a274b1e
DoMinhCat <mdo1@myges.fr>
2025-03-23 19:16:56 +0100

    fix

--- connexion/forgot_mdp.php
+++ connexion/forgot_mdp.php
@@ -3,61 +3,59 @@
 
 <?php
 $title = 'Mot de passe oublié';
-include('../include/head.php'); ?>
+include('../include/head.php'); 
+?>
 
 <body>
   <?php
   include("../include/header.php");
   ?>
 
-
   <div class="d-flex justify-content-center">
     <div class="col-8 col-sm-7 col-md-6 col-lg-7 col-xl-6 justify-content-center text-center p-5 my-5 connexion_box">
-    <?php if(!isset($_GET['success']) || empty($_GET['success'])): ?>
-      <div class="pb-1">
-        <h1 class="mb-0">Mot de passe oublié ?</h1>
-      </div>
 
-      <span class="lato16">Entrez votre email et nous vous enverrons un email pour réinitialiser votre mot de passe</span>
-
-      
-      <form method="post" action="forgot_verification.php">
-        <div class="d-flex flex-column pt-2 py-3 row-gap-1 lato16">
-          <input type="email" name="email" placeholder="Votre email" required aria-describedby="emailHelp" class="form-control input_field">
-
-          <div class="d-flex flex-column pt-3">
-            <input type="submit" class="btn btn-primary" value="Envoyer le lien de connexion">
-          </div>
-
-          <div class="lato24 pt-1">
-          <?php
-        if (isset($_GET['message']) && !empty($_GET['message'])) {
-          echo '<p>' . htmlspecialchars($_GET['message']) . '</p>';
-        }
-        ?>
-      </div>
-          
+      <?php if (!isset($_GET['success']) || empty($_GET['success'])): ?>
+        <div class="pb-1">
+          <h1 class="mb-0">Mot de passe oublié ?</h1>
         </div>
-      </form>
 
-      <div class="line-with-letters montserrat-titre32">
-        <span class="line"></span>OU<span class="line"></span>
-      </div>
+        <span class="lato16">Entrez votre email et nous vous enverrons un email pour réinitialiser votre mot de passe</span>
 
-      <div class="pb-2">
-        <a href="inscription.php" id="creer_compte">
-          Créer un compte
-        </a>
-      </div>
+        <form method="post" action="forgot_verification.php">
+          <div class="d-flex flex-column pt-2 py-3 row-gap-1 lato16">
+            <input type="email" name="email" placeholder="Votre email" required aria-describedby="emailHelp" class="form-control input_field">
 
-      <?php else:?>
+            <div class="d-flex flex-column pt-3">
+              <input type="submit" class="btn btn-primary" value="Envoyer le lien de connexion">
+            </div>
+
+            <div class="lato24 pt-1">
+              <?php
+              if (isset($_GET['message']) && !empty($_GET['message'])) {
+                echo '<p>' . htmlspecialchars($_GET['message']) . '</p>';
+              }
+              ?>
+            </div>
+
+          </div>
+        </form>
+
+        <div class="line-with-letters montserrat-titre32">
+          <span class="line"></span>OU<span class="line"></span>
+        </div>
+
+        <div class="pb-2">
+          <a href="inscription.php" id="creer_compte">
+            Créer un compte
+          </a>
+        </div>
+
+      <?php else: ?>
         <h2>Votre mot de passe a été réinitialisé</h2>
         <div class="line-with-letters py-2">
-        <span class="line"></span>
+          <span class="line"></span>
         </div>
-
-        <? endif;?>
-
+      <?php endif; ?>
 
       <div class="d-flex flex-column pt-1">
         <a href="login.php" class="btn btn-success">
