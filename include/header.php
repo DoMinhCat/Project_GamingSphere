05d9135bdc5bba850eb42ba3545b8aea9f0a12a3
DoMinhCat <mdo1@myges.fr>
2025-03-25 02:13:41 +0100

    small

--- include/header.php
+++ include/header.php
@@ -23,7 +23,7 @@ if (isset($_SESSION['user_id'])) {
 <header>
   <nav class="navbar nav-underline navbar-expand-xl bg-body-tertiary my-navbar" style="padding-top: 0.2rem; padding-bottom:0.4rem;">
     <div class="container container-fluid" style="padding: 0.5rem">
-      <a href=<?= ($this_page == 'index.php') ? 'connexion/login.php' : '../connexion/login.php' ?>>
+      <a href=<?= ($this_page == 'index.php') ? 'index.php' : '../index.php' ?>>
         <img src="<?= ($this_page == 'index.php') ? 'include/LOGO ENTIER 40px.png' : '../include/LOGO ENTIER 40px.png' ?>" alt="Logo" class="col-2 image-fluid" style="height: 30px; width: auto;">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
