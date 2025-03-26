91ae1d0271ab03358c97bfc70b9bb5e88c462ec3
DoMinhCat <mdo1@myges.fr>
2025-03-26 17:21:16 +0100

    footer+session timeout

---
+++ connexion/session_timeout.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Session expirée';
+include('../include/head.php');
+?>
+
+<body>
+    <div class="d-flex justify-content-center">
+        <div class="col-8 col-sm-7 col-md-6 col-lg-7 col-xl-6 justify-content-center text-center p-5 my-5 connexion_box">
+
+            <div class="pb-1">
+                <h1 class="mb-3">Vous êtes toujours là ?</h1>
+            </div>
+
+            <span class="lato24" style="color: #FF6E40;"><strong>Votre session a expirée.</strong></span><br>
+            <span class="lato16">Vous avez été déconnecté en raison d'inactivité afin de proteger vos informations privées</span>
+
+
+
+
+            <div class="line-with-letters montserrat-titre32 my-2">
+                <span class="line"></span>
+            </div>
+
+
+            <div class="d-flex flex-column pt-1">
+                <a href="login.php" class="btn btn-success">
+                    Revenir à la page de connexion
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    include("../include/footer.php");
+    ?>
+</body>
+
+</html>
--- include/footer.php
+++ include/footer.php
@@ -1,45 +1,45 @@
-<footer class="py-4" style="background-color: #1E3D59">
+<footer class="pb-4 pt-5">
     <div class="container">
-        <div class="row row-cols-1 row-cols-md-4 d-flex justify-content-between">
+        <div class="row row-cols-1 row-cols-md-4 d-flex justify-content-between m-2">
             <div class="col">
-                <h1 class="text-custom-color fs-5">À Propos</h1>
+                <h1 class=" fs-5 mb-4">À Propos</h1>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none text-light">Element1</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element2</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element3</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element4</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element1</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element2</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element3</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element4</a></li>
                 </ul>
             </div>
             <div class="col">
-                <h1 class="text-custom-color fs-5">Contactez-nous</h1>
+                <h1 class="fs-5 mb-4">Contactez-nous</h1>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none text-light">Element1</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element2</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element3</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element4</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element1</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element2</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element3</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element4</a></li>
                 </ul>
             </div>
             <div class="col">
-                <h1 class="text-custom-color fs-5">Aide</h1>
+                <h1 class="fs-5 mb-4">Aide</h1>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none text-light">Element1</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element2</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element3</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element4</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element1</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element2</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element3</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element4</a></li>
                 </ul>
             </div>
             <div class="col">
-                <h1 class="text-custom-color fs-5">Informations Légales</h1>
+                <h1 class="fs-5 mb-4">Informations Légales</h1>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none text-light">Element1</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element2</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element3</a></li>
-                    <li><a href="#" class="text-decoration-none text-light">Element4</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element1</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element2</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element3</a></li>
+                    <li class="py-2"><a href="#" class="text-decoration-none text-light">Element4</a></li>
                 </ul>
             </div>
         </div>
         <div class="text-center mt-4 text-white">
-            © MMP 
+            © MMP
         </div>
     </div>
 </footer>
