4b3b1a418bcf05ed5be2e2a684af5558cf16de71
Mrkikou78 <psainctavit1@myges.fr>
2025-03-26 16:52:25 +0100

    upadte messagerie

--- include/header.php
+++ include/header.php
@@ -1,5 +1,5 @@
 <?php
-include('../include/database.php');
+include('database.php');
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -19,34 +19,53 @@ if (isset($_SESSION['user_id'])) {
 } else {
   $notificationCount = 0;
 }
+$messagesNonLus = [];
+
+if (isset($_SESSION['user_id'])) {
+    try {
+        $stmtMessages = $bdd->prepare("SELECT m.id, m.expediteur_id, m.date_envoi
+                                      FROM messages m
+                                      WHERE m.destinataire_id = ? AND m.lu = 0
+                                      ORDER BY m.date_envoi DESC");
+        $stmtMessages->execute([$_SESSION['user_id']]);
+        $messagesNonLus = $stmtMessages->fetchAll(PDO::FETCH_ASSOC);
+        
+        $notificationCountMessages = count($messagesNonLus);
+    } catch (PDOException $e) {
+        $notificationCountMessages = 0;
+    }
+} else {
+    $notificationCountMessages = 0;
+}
+
 ?>
-<header>
-  <nav class="navbar nav-underline navbar-expand-xl bg-body-tertiary my-navbar" style="padding-top: 0.2rem; padding-bottom:0.4rem;">
+<header style="background-color: #ff6e40 !important;">
+  <nav class="navbar nav-underline navbar-expand-xl bg-body-tertiary my-navbar" style="padding-top: 0.2rem; padding-bottom:0.4rem;"> 
     <div class="container container-fluid" style="padding: 0.5rem">
-      <a href=<?= ($this_page == 'index.php') ? 'index.php' : '../index.php' ?>>
-        <img src="<?= ($this_page == 'index.php') ? 'include/LOGO ENTIER 40px.png' : '../include/LOGO ENTIER 40px.png' ?>" alt="Logo" class="col-2 image-fluid" style="height: 30px; width: auto;">
+      <a href="<?= ($this_page == 'index.php') ? '/PA/index.php' : '../index.php' ?>">
+        <img src="<?= ($this_page == 'index.php') ? '/PA/include/LOGO ENTIER 40px.png' : '../include/LOGO ENTIER 40px.png' ?>" alt="Logo" class="col-2 image-fluid" style="height: 30px; width: auto;">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-link lato16 <?= ($this_page == 'index.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? 'index.php' : '../index.php' ?> style="color: white; margin-right:1.5rem; margin-left:1.5rem;">
+          <a class="nav-link lato16 <?= ($this_page == 'index.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? '/PA/index.php' : '../index.php' ?> style="color: white; margin-right:1.5rem; margin-left:1.5rem;">
             Accueil
           </a>
-          <a class="nav-link lato16 <?= ($this_page == 'magasin_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? 'magasin/magasin_main.php' : '../magasin/magasin_main.php' ?> style="color: white;margin-right:1.5rem;">
+          <a class="nav-link lato16 <?= ($this_page == 'magasin_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? '/PA/magasin/magasin_main.php' : '../magasin/magasin_main.php' ?> style="color: white;margin-right:1.5rem;">
             Magasin
           </a>
-          <a class="nav-link lato16 <?= ($this_page == 'tournois_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? 'tournois/tournois_main.php' : '../tournois/tournois_main.php' ?> style="color: white;margin-right:1.5rem;">
+          <a class="nav-link lato16 <?= ($this_page == 'tournois_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? '/PA/tournois/tournois_main.php' : '../tournois/tournois_main.php' ?> style="color: white;margin-right:1.5rem;">
             Tournois
           </a>
-          <a class="nav-link lato16 <?= ($this_page == 'communaute_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? 'communaute/communaute_main.php' : '../communaute/communaute_main.php' ?> style="color: white;margin-right:1.5rem;">
+          <a class="nav-link lato16 <?= ($this_page == 'communaute_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? '/PA/communaute/communaute_main.php' : '../communaute/communaute_main.php' ?> style="color: white;margin-right:1.5rem;">
             Communauté
           </a>
-          <a class="nav-link lato16 <?= ($this_page == 'actualite_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? 'actualite/actualite_main.php' : '../actualite/actualite_main.php' ?> style="color: white;margin-right:1.5rem;">
+          <a class="nav-link lato16 <?= ($this_page == 'actualite_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? '/PA/actualite/actualite_main.php' : '../actualite/actualite_main.php' ?> style="color: white;margin-right:1.5rem;">
             Actualités
           </a>
-          <a class="nav-link lato16 <?= ($this_page == 'forum_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? 'forum/forum_main.php' : '../forum/forum_main.php' ?> style="color: white;">
+          <a class="nav-link lato16 <?= ($this_page == 'forum_main.php') ? 'active' : '' ?>" href=<?= ($this_page == 'index.php') ? '/PA/forum/forum_main.php' : '../forum/forum_main.php' ?> style="color: white;">
             Forum
           </a>
         </div>
@@ -65,10 +84,16 @@ if (isset($_SESSION['user_id'])) {
             </div>
           </form>
           <div class="d-flex align-items-center">
-            <a href="message.php" class="btn btn-outline-dark d-flex align-items-center me-3">
-              <i class="bi bi-chat-dots-fill"></i>
-            </a> 
-            <a href="panier.php" class="btn btn-outline-dark d-flex align-items-center me-3">
+          <div class="d-flex align-items-center">
+            <a href="/PA/messages/messagerie.php" class="btn btn-outline-dark d-flex align-items-center me-3 position-relative">
+                <i class="bi bi-chat-dots-fill"></i>
+                <?php if (isset($_SESSION['user_email']) && ($notificationCountMessages > 0)): ?>
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="font-size: 0.75rem;">
+                        <?= $notificationCountMessages ?>
+                    </span>
+                <?php endif; ?>
+            </a>
+                      <a href="panier.php" class="btn btn-outline-dark d-flex align-items-center me-3">
               <i class="bi bi-cart-fill"></i>
             </a>
               <?php if (isset($_SESSION['user_email'])) : ?>
@@ -127,7 +152,7 @@ if (isset($_SESSION['user_id'])) {
                   <a href="<?= $href ?>" class="dropdown-item btn btn-sm py-3"><?php echo htmlspecialchars($display_name) ?></a>
                 </li>
                 <li><button id="theme-btn" class="dropdown-item btn btn-sm py-3">Activer/Désactiver le mode nuit</button></li>
-                <li><a href="../connexion/deconnexion.php" class="dropdown-item btn btn-sm py-3">Se déconnecter</a></li>
+                <li><a href="/PA/connexion/deconnexion.php" class="dropdown-item btn btn-sm py-3">Se déconnecter</a></li>
               </ul>
             </div>
           </div>
