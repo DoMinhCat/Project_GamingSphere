30f4302bb6c13dc16c42e319a141feadf6200835
DoMinhCat <mdo1@myges.fr>
2025-03-31 17:51:23 +0200

    fix url

--- include/head.php
+++ include/head.php
@@ -6,13 +6,13 @@ $this_page = basename($_SERVER['PHP_SELF']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href=<?= ($this_page == 'index.php') ? '/PA/include/style.css' : '../include/style.css' ?>>
+    <link rel="stylesheet" href=<?= ($this_page == 'index.php') ? 'include/style.css' : '../include/style.css' ?>>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src=<?= ($this_page == 'index.php') ? '/PA/include/night_mode.js' : '../include/night_mode.js' ?>></script>
+    <script src=<?= ($this_page == 'index.php') ? 'include/night_mode.js' : '../include/night_mode.js' ?>></script>
     <title><?= $title ?></title>
 </head>
