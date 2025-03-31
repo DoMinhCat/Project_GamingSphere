85bf6caba08fc9d8367b363bef654be48a176824
DoMinhCat <mdo1@myges.fr>
2025-03-31 17:44:50 +0200

    dark mode text

--- include/night_mode.js
+++ include/night_mode.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("theme-btn");
-  const body = document.getElementsByTagNamebody;
+  const body = document.body;
+
+  function updateButtonText() {
+    toggleBtn.textContent = body.classList.contains("dark")
+      ? "Désactiver le mode nuit"
+      : "Activer le mode nuit";
+  }
 
   if (localStorage.getItem("theme") === "dark") {
     body.classList.add("dark");
   }
 
+  updateButtonText();
+
   toggleBtn?.addEventListener("click", () => {
     const isDark = body.classList.toggle("dark");
     localStorage.setItem("theme", isDark ? "dark" : "light");
+    updateButtonText();
   });
 });
