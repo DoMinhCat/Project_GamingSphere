3af4d06ae2a86514a67552176290e6c2d6a503a4
DoMinhCat <mdo1@myges.fr>
2025-04-06 21:18:24 +0200

    path timeout

--- include/check_timeout.js
+++ include/check_timeout.js
@@ -4,17 +4,12 @@ let timeout;
 function resetTimer() {
   clearTimeout(timeout);
   timeout = setTimeout(() => {
-    const path = window.location.pathname;
-    const page = path.substring(path.lastIndexOf("/") + 1);
-    if (page === "index.php") {
-      window.location.href = "connexion/session_timeout.php";
-    } else {
-      window.location.href = "../connexion/session_timeout.php";
-    }
+    const basePath = window.location.origin;
+    window.location.href = `${basePath}/connexion/session_timeout.php`;
   }, timeoutDuration);
 }
 
 window.onload = resetTimer;
-document.onmousemove = resetTimer;
-document.onkeydown = resetTimer;
-document.onclick = resetTimer;
+document.addEventListener("mousemove", resetTimer);
+document.addEventListener("keydown", resetTimer);
+document.addEventListener("click", resetTimer);
