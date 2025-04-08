edb5f815f2049ebec76d9f0d2e6cfa61730819ad
Mrkikou78 <psainctavit1@myges.fr>
2025-04-09 00:27:04 +0200

    upadte tournois (inscription, liste tournois, bo tournois)

--- profil/my_account.php
+++ profil/my_account.php
@@ -72,30 +72,11 @@ if (isset($_SESSION['user_email']) && !empty($_SESSION['user_email'])) {
 ?>
 
 <body>
-<?php include('../include/header.php'); ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light nav_profil">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-      <ul class="navbar-nav under_nav">
-        <li class="nav-item">
-          <a class="nav-link" href="friends_list.php">Liste des amis</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="tournaments_list.php">Mes tournois</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="order_history.php">Historique des commandes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/PA/connexion/deconnexion.php">Se déconnecter</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php 
+include('../include/header.php');
+include('navbar.php'); 
+?>
+
 
 <div class="container mt-4">
 
