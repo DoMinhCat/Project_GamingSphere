167c3e3c2031e24dda3ba49b4fe4404af89dac7e
DoMinhCat <mdo1@myges.fr>
2025-04-23 01:47:53 +0200

    url require path.php

--- back-office/index.php
+++ back-office/index.php
@@ -3,7 +3,7 @@ session_start();
 $login_page = '../connexion/login.php';
 require('check_session.php');
 require('../include/check_timeout.php');
-require_once('../path.php');
+require_once __DIR__ . '/../path.php';
 ?>
 
 <!DOCTYPE html>
