a7e5318431c2fe9a2ae8b2ac56e576f21f768497
DoMinhCat <mdo1@myges.fr>
2025-05-03 05:08:12 +0200

    url fix

--- actualite/actualite_article.php
+++ actualite/actualite_article.php
@@ -2,7 +2,7 @@
 session_start();
 require('../include/check_timeout.php');
 require('../include/database.php');
-require_once __DIR__ . '/../../path.php';
+require_once __DIR__ . '/../path.php';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
