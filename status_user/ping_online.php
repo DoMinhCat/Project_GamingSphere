390cf07b67c197062adac61bcd3610acd2c175a1
DoMinhCat <mdo1@myges.fr>
2025-05-03 17:16:51 +0200

    fix 3

--- status_user/ping_online.php
+++ status_user/ping_online.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 require('../include/database.php');
-if (!empty($_SESSION['user__email'])) {
+if (!empty($_SESSION['user_email'])) {
     $stmt = $bdd->prepare("UPDATE utilisateurs SET last_active = NOW() WHERE id = ?");
     $stmt->execute([$_SESSION['user_id']]);
 }
