c9c0f23f95da50a91703d51e02aede0f8f09f26e
DoMinhCat <mdo1@myges.fr>
2025-05-03 16:04:00 +0200

    .

--- status_user/ping_online.php
+++ status_user/ping_online.php
@@ -2,7 +2,7 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-require('/include/database.php');
+require('../include/database.php');
 if (!empty($_SESSION['user__email'])) {
     $stmt = $bdd->prepare("UPDATE utilisateurs SET last_active = NOW() WHERE id = ?");
     $stmt->execute([$_SESSION['user_id']]);
