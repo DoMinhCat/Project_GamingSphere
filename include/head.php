4b722ae6c5dae2c4dc365c204a143dfe84ee82b8
DoMinhCat <mdo1@myges.fr>
2025-05-04 00:41:27 +0200

    fix 3

--- include/head.php
+++ include/head.php
@@ -3,24 +3,30 @@ $this_page = basename($_SERVER['PHP_SELF']);
 
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
-
 ini_set('log_errors', 1);
 ini_set('error_log', '/var/log/apache2/php_errors.log');
 
 function custom_error_handler($errno, $errstr, $errfile, $errline)
 {
+    error_log("Error [$errno] $errstr in $errfile on line $errline");
+
     header("Location: /error_pages/500.php", true, 500);
     exit();
 }
 set_error_handler("custom_error_handler");
+
 register_shutdown_function(function () {
     $error = error_get_last();
     if ($error) {
+
+        error_log("Fatal Error: {$error['message']} in {$error['file']} on line {$error['line']}");
+
         header("Location: /error_pages/500.php", true, 500);
         exit();
     }
 });
 ?>
+
 
 <head>
     <meta charset="UTF-8">
