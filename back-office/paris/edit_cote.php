7127038f2b7a6866467cf6ba5ed932beeecad4b8
Mrkikou78 <psainctavit1@myges.fr>
2025-05-19 21:53:19 +0200

    Test pari pariable

--- back-office/paris/edit_cote.php
+++ back-office/paris/edit_cote.php
@@ -1,10 +1,9 @@
 <?php
 require('../../include/database.php');
+require('../check_session.php');
+require('../../include/check_timeout.php');
+require_once __DIR__ . '/../../path.php';
 session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    header('Location: paris.php');
-    exit();
-}
 if (isset($_POST['id_tournoi'], $_POST['cote'])) {
     $id_tournoi = intval($_POST['id_tournoi']);
     $cote = floatval($_POST['cote']);
--- back-office/paris/toggle_pariable.php
+++ back-office/paris/toggle_pariable.php
@@ -1,15 +1,15 @@
 <?php
 require('../../include/database.php');
+require('../check_session.php');
+require('../../include/check_timeout.php');
+require_once __DIR__ . '/../../path.php';
 session_start();
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    header('Location: paris.php');
-    exit();
-}
 if (isset($_POST['id_tournoi'], $_POST['pari_ouvert'])) {
     $id_tournoi = intval($_POST['id_tournoi']);
     $pari_ouvert = intval($_POST['pari_ouvert']) ? 1 : 0;
     $stmt = $bdd->prepare("UPDATE tournoi SET pari_ouvert = ? WHERE id_tournoi = ?");
     $stmt->execute([$pari_ouvert, $id_tournoi]);
 }
-header('Location: paris.php?message=EDIT_ME');
+
+header('Location: paris.php');
 exit();
