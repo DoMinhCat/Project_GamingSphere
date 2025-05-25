f7331096d57de1e2cdf4e8e727c0d11263524b53
Mrkikou78 <psainctavit1@myges.fr>
2025-05-25 15:47:23 +0200

    fix edit_cote

--- back-office/paris/edit_cote.php
+++ back-office/paris/edit_cote.php
@@ -5,19 +5,19 @@ require('../check_session.php');
 require('../../include/check_timeout.php');
 require_once __DIR__ . '/../../path.php';
 
-if (isset($_POST['id_tournoi'], $_POST['id_team'], $_POST['cote'])) {
+if (isset($_POST['id_tournoi'], $_POST['id_team'], $_POST['cote'], $_POST['id_user'])) {
     $id_tournoi = intval($_POST['id_tournoi']);
     $id_team = intval($_POST['id_team']);
+    $id_user = intval($_POST['id_user']);
     $cote = floatval($_POST['cote']);
     if ($cote < 1) $cote = 1;
 
-    // Upsert : insert ou update la cote du participant pour ce tournoi
     $stmt = $bdd->prepare("
-        INSERT INTO cote_participant (id_tournoi, id_team, cote)
-        VALUES (?, ?, ?)
+        INSERT INTO cote_participant (id_tournoi, id_team, id_user, cote)
+        VALUES (?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE cote = VALUES(cote)
     ");
-    $stmt->execute([$id_tournoi, $id_team, $cote]);
+    $stmt->execute([$id_tournoi, $id_team, $id_user, $cote]);
 }
 
 header('Location: paris.php?message=EDIT_ME');
